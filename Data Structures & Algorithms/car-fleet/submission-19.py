12ed38b1bc9aa54b8596b6842398014cc3419aa1
Subeen Regmi <72048704+subeenregmi@users.noreply.github.com>
2026-08-16 12:21:49 +0100

    Add: car-fleet - submission-19

---
+++ Data Structures & Algorithms/car-fleet/submission-19.py
@@ -0,0 +1,20 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pos_speed = zip(position, speed)
+        pos_speed = sorted(pos_speed, key=lambda p: -p[0])
+
+        stack = []
+
+        for ps in pos_speed:
+            if len(stack) == 0:
+                stack.append(ps)
+                continue
+            else:
+                head = stack[-1]
+                head_time_to_dest = (target - head[0]) / head[1]
+                current_time_to_dest = (target - ps[0]) / ps[1]
+
+                if current_time_to_dest > head_time_to_dest:
+                    stack.append(ps)
+
+        return len(stack)
